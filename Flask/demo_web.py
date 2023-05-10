bc2dc29dd2cbf91faa56e0c59237c7dfa5c59a2d
GGBoomSiege <396947793@qq.com>
2023-05-10 17:30:55 +0800

    Python Flask

--- Flask/demo_web.py
+++ Flask/demo_web.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/show/info")
+@app.route("/")
 def index():
-    return "中国联通"
+    return render_template('index.html')
+
+@app.route("/get/news")
+def get_news():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
