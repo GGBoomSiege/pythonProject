c1c0f316a1bba270aa1af6663c0a3d350ebf5bf8
GGBoomSiege <396947793@qq.com>
2023-06-15 17:27:18 +0800

    数据清洗01

--- Django/data_presentation/dataPage/get_jobs.py
+++ Django/data_presentation/dataPage/get_jobs.py
@@ -18,7 +18,7 @@ def get_jobs(JOB_KEY):
     # options.add_argument('--disable-dev-shm-usage')
 
     # options = webdriver.ChromeOptions()
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     options.add_argument(f"user-agent={user_agent}")
     service = webdriver.chrome.service.Service(executable_path=r"D:\Python-3.11\chromedriver.exe")
     service_log_path = 'chromedriver.log'
