eccf8d081be7b647e579295f12bef3cd05d915e1
GGBoomSiege <88217512+GGBoomSiege@users.noreply.github.com>
2023-10-27 11:57:26 +0800

    Update demo01.py

--- chatgpt/demo01/demo01.py
+++ chatgpt/demo01/demo01.py
@@ -1,7 +1,7 @@
 import openai
 
-openai.organization = "org-q4K1V5Usbd1b8v7aqmHlNBaM"
-openai.api_key = "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"
+openai.organization = ""
+openai.api_key = ""
 response = openai.Image.create(
   prompt="A fluffy white cat with blue eyes sitting in a basket of flowers, looking up adorably at the camera",
   n=1,
