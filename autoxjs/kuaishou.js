e3c3affb887d33e46b5197dfeb26a604301dc224
GGBoomSiege <88217512+GGBoomSiege@users.noreply.github.com>
2024-08-18 22:25:01 +0800

    update

--- autoxjs/kuaishou.js
+++ autoxjs/kuaishou.js
@@ -63,18 +63,36 @@ function waitForRegionImage(image, x, y, width, height, timeout, threshold) {
   }
 }
 
+function backMain(package) {
+  openAppSetting(package);
+  sleep(2000);
+  id("action_menu_item_child_text").className("android.widget.TextView").text("结束运行").findOne().parent().click();
+  sleep(2000);
+  click("确定");
+  sleep(2000);
+  back();
+}
+
+function runMain(package) {
+  app.launch(package);
+  waitForPackage(package);
+  sleep(3000);
+  back();
+}
+
 //   看广告赚金币
 
 function kuaishou_ad() {
   log("广告开始");
-  while (true) {
+  var ad_count = 0;
+  while (ad_count++ < 30) {
     var kuaishou_ad_success = images.read("./kuaishou/kuaishou_ad_success.png");
     var kuaishou_ad_success_point = waitForGrayscaleImage(kuaishou_ad_success, 60000);
     kuaishou_ad_success.recycle();
 
     if (kuaishou_ad_success_point) {
       click(kuaishou_ad_success_point.x, kuaishou_ad_success_point.y);
-      sleep(2000);
+      sleep(1000);
 
       var kuaishou_ad_continue = images.read("./kuaishou/kuaishou_ad_continue.png");
       var kuaishou_ad_continue_point = waitForGrayscaleImage(kuaishou_ad_continue, 5000);
@@ -82,7 +100,6 @@ function kuaishou_ad() {
 
       if (kuaishou_ad_continue_point) {
         click(kuaishou_ad_continue_point.x, kuaishou_ad_continue_point.y);
-        sleep(2000);
       }
     }
 
@@ -97,8 +114,9 @@ function kuaishou_ad() {
   }
 }
 
-function main() {
-  while (true) {
+function run() {
+  var run_count = 0;
+  while (run_count++ < 20) {
     var kuaishou_ad_flag = images.read("./kuaishou/kuaishou_ad_flag.png");
     var kuaishou_ad_flag_point = waitForImage(kuaishou_ad_flag, 3000);
     kuaishou_ad_flag.recycle();
@@ -112,6 +130,7 @@ function main() {
       if (kuaishou_ad_startup_point) {
         sleep(3000);
         click(kuaishou_ad_startup_point.x, kuaishou_ad_startup_point.y);
+        sleep(3000);
         kuaishou_ad(); // 点击广告
       }
     } else {
@@ -119,8 +138,34 @@ function main() {
       break;
     }
 
-    sleep(1000);
+    sleep(2000);
+    if (count === 20) {
+      log("脚本运行失败");
+    }
   }
+}
+
+function main() {
+  runMain("com.kuaishou.nebula");
+  sleep(5000);
+  backMain("com.kuaishou.nebula");
+
+  sleep(5000);
+  runMain("com.kuaishou.nebula");
+  sleep(5000);
+  backMain("com.kuaishou.nebula");
+
+  sleep(5000);
+  runMain("com.kuaishou.nebula");
+  sleep(5000);
+  // 点击去赚钱
+  click(756, 2230);
+  sleep(10000);
+  swipe((2 / 3) * x, (2 / 3) * y, (2 / 3) * x, (1 / 2) * y, 500);
+  sleep(2000);
+  run();
+  sleep(5000);
+  backMain("com.kuaishou.nebula");
 }
 
 if (!requestScreenCapture()) {
@@ -130,8 +175,5 @@ if (!requestScreenCapture()) {
 
 const x = 1080;
 const y = 2340;
-
-const startTime = Date.now();
-const timeout = 150 * 60 * 1000;
 
 main();
